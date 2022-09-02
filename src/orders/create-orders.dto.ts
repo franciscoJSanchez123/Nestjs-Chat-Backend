d17a239de8b4856878c526b0e1639dfd9cef251f
franciscoJSanchez123 <javisan0105@gmail.com>
2022-09-02 15:56:19 -0430

    changes

--- src/orders/create-orders.dto.ts
+++
@@ -1,9 +0,0 @@
-import {Users} from './../users/users.entity'
-
-export class CreateOrderDto {
-    orderId?:string;
-    user: Users;
-    items:[];
-    total:number;
-    status:string
-  }
--- src/users/create-users.dto.ts
+++
@@ -1,8 +0,0 @@
-export class CreateUsersDto {
-    _id?:string;
-    name: string;
-    password: string;
-    email:string;
-    ordersId:string[]
-    admin:boolean
-  }
